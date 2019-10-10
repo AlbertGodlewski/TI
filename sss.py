cc51a487200373048ffb1768fe63f76c5cec976c
Albert Godlewski <albertosfx@wp.pl>
2019-10-10 10:06:24 +0200

    sas

---
+++ sss.py
@@ -0,0 +1,3 @@
+print("Cyfra {} poprzedza {}".format(7,8))
+print("Rekord świata na 100m to {:.2f} ustanowił go {}".format(9.58, 'Usain Bolt'))
+print("Rekord świata na 100m to %.2f ustanowił go %s" % (9.58, 'Usain Bolt'))
